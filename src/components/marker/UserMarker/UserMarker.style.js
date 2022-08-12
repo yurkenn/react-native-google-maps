3459f46a728eef7603751e88841c329de9728e75
oguz yurken <oguz.yurken@gmail.com>
2022-08-13 01:57:24 +0300

    Update UserMarker

--- src/components/marker/UserMarker/UserMarker.style.js
+++ src/components/marker/UserMarker/UserMarker.style.js
@@ -2,8 +2,8 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   image: {
-    width: 50,
-    height: 50,
+    width: 40,
+    height: 40,
     borderRadius: 25,
     borderWidth: 1,
     borderColor: '#e0e0e0e0',
